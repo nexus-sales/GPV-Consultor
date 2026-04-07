26ba82538912cf4fe8201579b5eb666d734ba40b
Salvador Muñoz Portillo <admin@nexus-sales.eu>
2026-04-07 16:42:47 +0100

    fix: Corrected ReactQueryDevtools export name in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -9,7 +9,7 @@ import { SyncQueueProvider } from './lib/hooks/useSyncQueue'
 import { ConfirmProvider } from './lib/ConfirmProvider'
 import { ErrorBoundary } from './components/ErrorBoundary'
 import { QueryClientProvider } from '@tanstack/react-query'
-import { ReactQueryDevTools } from '@tanstack/react-query-devtools'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { queryClient } from './lib/queryClient'
 import './lib/config'
 import './styles.css'
@@ -82,7 +82,7 @@ createRoot(rootElement).render(
           </ThemeProvider>
         </AuthProvider>
       </ErrorBoundary>
-      <ReactQueryDevTools initialIsOpen={false} />
+      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   </React.StrictMode>
 )
